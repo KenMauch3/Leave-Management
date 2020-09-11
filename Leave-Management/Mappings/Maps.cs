eb1793dcbd40b3c573b5c58ad116c4e94de55d6f
Ken Mauch <kmauch@shoptech.com>
2020-09-11 14:50:49 -0400

    Add project files.

---
+++ Leave-Management/Mappings/Maps.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Leave_Management.Data;
+using Leave_Management.Models;
+using Leave_Management.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Management.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType, DetailsLeaveTypeViewModel>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeViewModel>().ReverseMap();
+            CreateMap<Employee, EmployeeViewModel>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAlloactionViewModel>().ReverseMap();
+            CreateMap<LeaveHistory, LeaveHistoryViewModel>().ReverseMap();
+        }
+    }
+}
